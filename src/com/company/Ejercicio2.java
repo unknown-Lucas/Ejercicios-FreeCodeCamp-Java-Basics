a9d1fadc7968adf8fe3e01b8b0495c9fff4320b8
Lucas Coronilla <lucassalesianos@gmail.com>
2022-07-06 13:29:54 +0200

    aaas

---
+++ src/com/company/Ejercicio2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Ejercicio2 {
+    public static void main(String[] args){
+        System.out.println(iva(2));
+    }
+    public static double iva(double args){
+        return args * 1.21;
+    }
+}
---
+++ src/com/company/Ejercicio3.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Arrays;
+import java.util.List;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args){
+        String[] names = {"Pepe", "Juan", "Anthony"};
+        concat(names);
+    }
+
+    public static void concat(String[] namesArr){
+
+        List<String> namesList = Arrays.asList(namesArr);       //Array to list
+        StringBuffer namesConcat = new StringBuffer();      //To modify strings use StringBuffer;
+        namesList.forEach((String name)->{      //We can use foreach instead fop variable is a list.
+            namesConcat.append(name).append(" ");
+        });
+        namesConcat.deleteCharAt(namesConcat.length()-1);
+        System.out.println(namesConcat);
+    }
+
+}
